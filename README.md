947648c2fd64688102755ea0464d32251fc3eef1
Keertan Dakarapu <keertan.dakarapu@copart.com>
2018-02-02 10:32:53 -0600

    supports models

--- README.md
+++ README.md
@@ -2,35 +2,35 @@
 Using swagger_ui to feed a custom json
 
 
+```
 
   Service Name :: Type
   post              1
   get               2
   put               3
   delete            4
+  
 
-
+```
 
 
 Example format: 
 
-"
+```
+	 #body parameters - the next line should follow with parameters in given format, and this line should contain 'body parameters'
+	 # name : Array, lot : string
+	 post '/lots/accept' do # used for accept lot information (Summary)  
+	    return_errors(lots: I18n.t('common.errors.required')) unless params[:lots] && params[:lots].is_a?(Array) 
+	    params[:lots].each { |number| Ycs::Transporter::UpdateLot.accepted(number) } ## double hashes for internal comments
+	    { status: 'success' }.to_json
+	  end
 
- #body parameters - the next line should follow with parameters in given format
- # name : Array, lot : string
- post '/lots/accept' do # used for accept lot information (Summary)  
-    return_errors(lots: I18n.t('common.errors.required')) unless params[:lots] && params[:lots].is_a?(Array) ## double hashes for internal comments
-    params[:lots].each { |number| Ycs::Transporter::UpdateLot.accepted(number) }
-    { status: 'success' }.to_json
-  end
+	  # Anything written before the next service will be added to its description 
 
-  # Anything written before the next service will be added to its description 
+	 post '/lots/reject' do #writing summary here is unknown to aliens # in summary just include -d to make it depricated
+	   return_errors(lots: I18n.t('common.errors.required')) unless params[:lots] && params[:lots].is_a?(Array)
+	   params[:lots].each { |number| Ycs::Transporter::UpdateLot.rejected(number) }
+	   { status: 'success' }.to_json
+	  end
 
-  post '/lots/reject' do #writing summary here is unknown to aliens # in summary just include -d to make it depricated
-    return_errors(lots: I18n.t('common.errors.required')) unless params[:lots] && params[:lots].is_a?(Array)
-    params[:lots].each { |number| Ycs::Transporter::UpdateLot.rejected(number) }
-    { status: 'success' }.to_json
-  end
-
-
-  "
+```
--- Swagger_json_creator.rb
+++ Swagger_json_creator.rb
@@ -4,11 +4,13 @@ require 'json'
 
 
 class Service
-  @@baseurl = ""
   @@all_tags = %w[]
+  @@baseurl = ""
+  @@host = ""
+  @@schema_global = []
 
-  attr_reader :name, :tag, :comment, :summary, :paramset, :path, :name, :type, :param_flag
-  attr_accessor :deprecated, :body_params
+  attr_reader :name, :tag, :comment, :summary, :paramset, :path, :name, :type, :param_flag, :response_codes
+  attr_accessor :deprecated, :body_params, :id
 
   def intialize(default_name="post", type = 0, path = "/", deprecated = false, param_flag = false, paramset = %w[], default_comment = "", default_summary = "Not provided")
     @name = default_name
@@ -22,8 +24,10 @@ class Service
     @summary = default_summary
     @testing_lotno = 12345
     @body_params = {}
+    @response_codes = {}
+    @@host = "0.0.0.0:9292"
     @@baseurl = "0.0.0.0:9292/transporter/"
-
+    id = "0"
   end
 
   def extraction line
@@ -80,8 +84,35 @@ class Service
     @body_params = params
   end
 
+  def attach_response_codes codes
+    @response_codes = codes
+  end
+
+  def self.add_to_global_schema schema
+    @@schema_global << schema
+  end
+
+  def self.check_duplicate_schema schema
+    #@@schema_global&.each do |existing| 
+    #  if existing==schema  
+    #    return true
+    #  else
+    #    return false 
+    #  end
+    #end
+    false
+  end
+
   def self.baseurl=(url)
     @@baseurl = url
+  end
+
+  def self.host=(host)
+    @@host = host
+  end
+
+  def self.schema_global=(schema)
+    @@schema_global = schema
   end
 
   def self.all_tags=(given_all_tags)
@@ -94,6 +125,14 @@ class Service
 
   def self.baseurl
     @@baseurl
+  end
+
+  def self.host
+    @@host
+  end
+
+  def self.schema_global
+    @@schema_global
   end
 
   def self.all_tags
@@ -114,18 +153,32 @@ end
 
 #Independent functions  ************************************
 
+def capture_host_and_baseurl line
+  line.include?("host") ? Service.host = line[/host = \'(.+)\'/,1] : true
+  line.include?("baseurl") ? Service.baseurl = line[/baseurl = \'(.+)\'/,1] : true
+end
+
 def extract_title url
   url.split('/').last.chomp('.rb') || "Unknown API"
+end
+
+def extract_response_code line
+  temp_hash = {}  
+  temp_hash[line.sub("#","").split(":")[0]&.strip] = { "description" => line.sub("#","").split(":")[1]&.strip }
+  temp_hash
 end
 
 def hotword? line
   /(post|POST|get|GET|DELETE|delete|put|PUT)\s(.*)/.match(line)
 end
 
-
 def single_commented? line
   temp = line.lstrip
   temp[0] == '#' && temp[1] != '#'? true : false
+end
+
+def response_definition_toggle? line
+  line.strip[0] == "#" && line.strip[1] == "-"
 end
 
 def double_commented? line
@@ -145,7 +198,7 @@ def format_summary line
   line.scan(/#.+/)[0]&.sub('#','') || "Summary not provided"
 end
 
-def find_deprecated? line
+def deprecated? line
   line.include?("-d") || line.include?("deprecated")
 end
 
@@ -165,7 +218,104 @@ def tag_array_creator tags
   tag_array
 end
 
-def generate_parameter s
+def body_params? line
+  line.include?("body_parameters") || line.include?("body parameters") || line.include?("body params") || line.include?("bodyparameters")
+end
+
+def extract_body_params line
+  body_params = []
+  line = line.to_s.sub('#','').strip
+  line.split(',').each do |paramset|
+    temp_hash = {}
+    temp_hash["name"] = paramset.split(':')[0].strip
+    if paramset.split(':')[1].strip.include? "array"
+      temp_array_definition = paramset.split(':')[1].strip
+      temp_hash["type"] = "array"
+      temp_hash["items"] = { "type" => temp_array_definition[/\[(.+)\]/,1] }
+    else
+      temp_hash["type"] = paramset.split(':')[1].strip
+    end
+    paramset.split(':')[2]&.strip ? temp_hash["required"] = true : temp_hash["required"] = false   
+    body_params << temp_hash
+  end
+  body_params
+end
+
+def generate_array_schema_internal(array_parameter, schema_name)
+  path = "#/definitions/"
+  schema = {}
+  schema["type"] = "array"
+  schema["items"] = { "type" => array_parameter[0]["items"]["type"] }
+
+  ####### Should work this out!!
+  final_schema = {}
+  final_schema[schema_name] = schema
+  if Service.check_duplicate_schema final_schema
+    return Service.check_duplicate_schema final_schema
+  else
+    Service.add_to_global_schema final_schema
+  end
+
+  puts "output is: " , final_schema
+  path + schema_name
+end
+
+def basic_datatypes? data_type
+  data_type.casecmp("integer") || data_type.casecmp("string")
+end
+
+def existing_type? data_type
+  #implement checking it
+end
+
+def get_schema_reference data_type
+  #implement fetching schemas here
+end
+
+def generate_schema_internal(body_parameters_internal_array, schema_name)
+  path = "#/definitions/"
+  schema = {}
+  schema["type"] = "object"
+  temp_hash = {}
+  required_array = []
+
+  body_parameters_internal_array.each do |body_param|    
+    if body_param["required"]
+      required_array << body_param["name"]
+    end
+
+    if body_param["type"] == "array" # contains array, then
+      #temp_hash[body_param["name"]] = { "$ref" => generate_array_schema_internal( [{ "name" => body_param["name"], "items" => body_param["items"]  }], body_param["name"] ) }
+      if basic_datatypes? body_param["items"]["type"]
+        temp_hash[body_param["name"]] = { "type" => "array", "items" => { "type" => body_param["items"]["type"]} }
+      elsif c? body_param["items"]["type"]
+        temp_hash[body_param["name"]] = { "type" => "array", "items" => { "type" => get_schema_reference(body_param["items"]["type"]) } }
+      else
+        # create new schema
+        #temp_hash[body_param["name"]] = { "$ref" => generate_array_schema_internal( [{ "name" => body_param["name"], "items" => body_param["items"]  }], body_param["name"] ) }
+      end  
+    else
+      temp_hash[body_param["name"]] = 
+                                  { 
+                                    "type" => body_param["type"]
+                                  }
+    end
+  end
+
+  puts required_array.inspect
+  required_array&.empty? ? false : schema["required"] = required_array 
+  schema["properties"] = temp_hash
+  final_schema = {}
+  final_schema[schema_name] = schema
+  if Service.check_duplicate_schema final_schema
+    return Service.check_duplicate_schema final_schema
+  else
+    Service.add_to_global_schema final_schema
+  end
+  path + schema_name
+end
+
+def generate_parameter_internal s
   temp_array = []
   s.paramset&.each do |inline_param|
     temp_hash = {}  
@@ -176,16 +326,38 @@ def generate_parameter s
     temp_array << temp_hash
   end
 
-  s.body_params&.each do |body_param|
-    temp_hash = {}
-    temp_hash["name"] = body_param["name"]
-    temp_hash["in"] = "body"
-    temp_hash["required"] = false
-    temp_hash["type"] = body_param["type"]
-    temp_array << temp_hash
-  end
+  if (s.type == 1 || s.type == 3) && !s.body_params.nil? # only for post or put services only schema is defined externally, or else it is defined in response
+    body_parameters_internal_array = []
+   
+    s.body_params&.each do |body_param|
+      temp_hash = {}
+      temp_hash["name"] = body_param["name"]
+      temp_hash["in"] = "body"
+      temp_hash["required"] = body_param["required"] || false
+      temp_hash["type"] = body_param["type"]
+      body_param["items"] ? temp_hash["items"] = body_param["items"] : true
+      body_parameters_internal_array << temp_hash
+    end
 
+    body_param_object = {}
+    body_param_object["in"] = "body"
+    body_param_object["name"] = "body"
+    body_param_object["description"] = "parameters to be sent in request"
+    body_param_object["required"] = true
+    if s.type == 1 || s.type == 3 
+      body_param_object["schema"] = { "$ref" => generate_schema_internal(body_parameters_internal_array, s.tag + s.id) }
+    end
+    temp_array << body_param_object
+  end
   temp_array
+end
+
+def generate_response_internal service_obj
+  temp_hash = {}
+  service_obj.response_codes&.each do |code|  
+    temp_hash = temp_hash.merge(code)
+  end
+  temp_hash
 end
 
 def generate_service_internal service_obj
@@ -195,7 +367,8 @@ def generate_service_internal service_obj
   temp_hash["description"] = service_obj.comment
   temp_hash["consumes"] = %w[ application/json application/xml]
   temp_hash["produces"] = %w[ application/json application/xml]
-  temp_hash["parameters"] = generate_parameter service_obj
+  temp_hash["parameters"] = generate_parameter_internal service_obj
+  temp_hash["responses"] = generate_response_internal service_obj
   temp_hash["deprecated"] = service_obj.deprecated ? true : false
   temp_hash
 end
@@ -219,23 +392,31 @@ end
 
 def generate_security_definitions
   # still to code
+  {
+    "petstore_auth":
+      {"type":"oauth2",
+        "authorizationUrl":"http://petstore.swagger.io/oauth/dialog",
+        "flow":"implicit",
+        "scopes":
+          {"write:pets":"modify pets in your account",
+            "read:pets":"read your pets"
+          }
+      },
+      "api_key":
+        {"type":"apiKey","name":"api_key","in":"header"}
+  }
+  
 end
 
-def body_params? line
-  line.include?("body_parameters") || line.include?("body parameters") || line.include?("body params") || line.include?("bodyparameters")
-end
-
-def extract_body_params line
-  body_params = []
-  line = line.to_s.sub('#','').strip
-  line.split(',').each do |paramset|
-    temp_hash = {}
-    temp_hash["name"] = paramset.split(':')[0].strip
-    temp_hash["type"] = paramset.split(':')[1].strip
-    body_params << temp_hash
+def generate_definitions
+  temp_hash = {}
+  Service.schema_global.each do |schema|
+    temp_hash = temp_hash.merge(schema)
   end
-  body_params
+  puts "definitions: " ,temp_hash
+  temp_hash
 end
+
 
 def create_json(title, host = "127.0.0.1", baseurl = "/", tag_and_path_based_service_order)
   js = 
@@ -259,9 +440,7 @@ def create_json(title, host = "127.0.0.1", baseurl = "/", tag_and_path_based_ser
   js["schemes"] = %w[ http ]
   js["paths"] = generate_internal(tag_and_path_based_service_order)
   js["securityDefinitions"] = generate_security_definitions 
-
-  #include a method to do curl calls and get models to add.
-
+  js["definitions"] = generate_definitions
   js.to_json
 end
 
@@ -270,11 +449,12 @@ end
 if ARGV.empty?
   puts "Error: Specify file location as argument"
 else
-  Service.baseurl = "hello/sample"
   service_count = 0
   comment_accumulator = ""
+  response_accumulator = []
   current_body_params = {}
   body_params_flag = false
+  response_code_flag = false
   tag_and_path_based_service_order = {}
   $services_array = Array.new # Declare a global array for handling services objects created
 
@@ -286,23 +466,36 @@ else
 
   File.readlines(ARGV[0].to_s).each do |line|
     if single_commented? line #check if its commented
-      if !body_params_flag && body_params?(line)
+      case
+      when line.include?("host") || line.include?("baseurl")
+        capture_host_and_baseurl line
+      when !body_params_flag && body_params?(line)
         body_params_flag = true
-        puts "params detected"
-      elsif body_params_flag
+      when body_params_flag
         current_body_params = extract_body_params line
-        body_params_flag = false 
+        body_params_flag = false
+      when response_definition_toggle?(line)
+        if response_code_flag
+          response_code_flag = false
+          $services_array[service_count-1].attach_response_codes response_accumulator
+          response_accumulator = []
+        else
+          response_code_flag = true
+        end
+      when response_code_flag && extract_response_code(line) != ""
+        response_accumulator << extract_response_code(line)
       else
         comment_accumulator += format_comment(line)
       end
     elsif hotword? line
       s = Service.new
       service_count = service_count + 1
+      s.id = service_count.to_s
       s.attach_body_params current_body_params
       s.extraction line      
       s.comment = comment_accumulator
       s.summary = format_summary line
-      s.deprecated = find_deprecated? s.summary
+      s.deprecated = deprecated? s.summary
       comment_accumulator = ""
       $services_array.push(s)
       
@@ -319,18 +512,17 @@ else
         end
       end
       s = nil
-      current_body_params = nil
+      current_body_params = nil      
     elsif double_commented? line
       $services_array[service_count-1].comment += format_internal_comment line
     end
   end  
 end
 
-puts service_count.to_s + " Routes Discovered"
-
 #Generate json file
-puts create_json(title, '0.0.0.0', Service.baseurl, tag_and_path_based_service_order)
+puts create_json(title, Service.host , Service.baseurl, tag_and_path_based_service_order)
 
+#print the json into an output file
 
 
 
